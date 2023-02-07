184f3b0197444479e14a73928e9d180277bd80d9
marcor93 <mrizzo93@gmail.com>
2023-02-07 15:59:52 -0800

    finished laying out and styling header and footer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,22 @@
 import "./Header.scss";
+import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
     <section className="header">
-      <p>This is a Header</p>
+      <div className="header__container">
+        <h1 className="header__title">FitCal</h1>
+        <div className="header__right">
+          <p className="header__right--date">
+            {new Date(Date.now()).toLocaleDateString("en-US", {
+              day: "2-digit",
+              month: "2-digit",
+              year: "numeric",
+            })}
+          </p>
+          <p className="header__right--user">Hello PlaceHolder</p>
+        </div>
+      </div>
     </section>
   );
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,7 @@
+import "./NavBar.scss";
+
+function NavBar() {
+  return <p>Test nave</p>;
+}
+
+export default NavBar;
